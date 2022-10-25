11ca30074fd71109754b577e7dce8009657ac9f3
TH-Raju <rajukhan894200@gmail.com>
2022-10-25 12:41:07 +0600

    project setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCfndKPsCdQZfWVp-6H2i5Wf7f1Hmt5IGs",
+    authDomain: "education-unlimited-7607f.firebaseapp.com",
+    projectId: "education-unlimited-7607f",
+    storageBucket: "education-unlimited-7607f.appspot.com",
+    messagingSenderId: "348498160905",
+    appId: "1:348498160905:web:81ac653a5e5940367237d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
