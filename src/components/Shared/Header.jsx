54738a5b84e33f4b562df682227a81906f6beb85
TH-Raju <rajukhan894200@gmail.com>
2022-10-26 19:48:33 +0600

    debugging

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -80,7 +80,7 @@ const Header = () => {
                                             <Link onClick={handleLogOut} className="block py-2 pr-4 pl-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Log Out</Link>
                                         </li>
                                         <li>
-                                            <Link className="inline py-2 pr-4 pl-3 text-blue-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{user?.uid ? user.displayName
+                                            <Link className="inline py-2 pr-4 pl-3 text-blue-700 rounded hover:bg-gray-100 md:hidden lg:block sm:block md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{user?.uid ? user.displayName
                                                 : user.email}</Link>
                                         </li>
                                     </>
