0d8a7650c255587a218a4fa55ce415e2a5ab2799
TH-Raju <rajukhan894200@gmail.com>
2022-10-26 21:47:49 +0600

    responsive setup

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -5,7 +5,7 @@ import LeftSide from './Shared/LeftSide';
 const Courses = () => {
     return (
 
-        <div className='grid grid-cols-3 gap-2 sm:mt-20 lg:mx-32 md:mx-10'>
+        <div className='grid md:grid-cols-3 sm:grid-cols-1 gap-2 sm:mt-20 lg:mx-32 md:mx-10'>
             <div className='bg-gray-200'>
                 <LeftSide></LeftSide>
             </div>
