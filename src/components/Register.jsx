685311b1bd2dc4f16b9a8c206b37fbfe7d2ecadc
TH-Raju <rajukhan894200@gmail.com>
2022-10-27 00:11:25 +0600

    page debuggin

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -57,6 +57,7 @@ const Register = () => {
     return (
         <div className='lg:mx-32 md:mx-10'>
             <form className='w-80 mx-auto' onSubmit={handleSubmit}>
+                <h1 className='text-4xl font-semibold text-center'>Registration</h1>
                 <div className="mb-6">
                     <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
                     <input type="name" id="name" name='name' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Your Name" required />
