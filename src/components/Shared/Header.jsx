99f624fcc9250b68ef9acba4e4046c6a3f64fc45
TH-Raju <rajukhan894200@gmail.com>
2022-10-26 22:13:21 +0600

    responsive issue debugging

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
                                     </button>
                             }
                         </span>
-                        {/* <button onClick={() => setToggle(!toggle)} type="button" className='ml-3'>
+                        <button onClick={() => setToggle(!toggle)} type="button" className='ml-3 hidden md:block '>
                             {
                                 toggle ?
                                     <>
@@ -54,7 +54,7 @@ const Header = () => {
                                         <div> <CiDark />light</div>
                                     </>
                             }
-                        </button> */}
+                        </button>
                         <button onClick={() => setOpen(!open)} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="true">
                             <svg className="w-8 h-8" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                         </button>
@@ -73,15 +73,15 @@ const Header = () => {
                             <li>
                                 <Link to="/faq" className="block py-2 pr-4 pl-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">FAQ</Link>
                             </li>
-                            <button onClick={() => setToggle(!toggle)} type="button" className='ml-3'>
+                            <button onClick={() => setToggle(!toggle)} type="button" className='ml-3 lg:hidden bg-cyan-600 md:hidden flex items-center py-2 px-2 w-20 rounded-xl gap-2'>
                                 {
                                     toggle ?
                                         <>
-                                            <div><MdNightlightRound />dark</div>
+                                            <MdNightlightRound />dark
                                         </>
                                         :
                                         <>
-                                            <div> <CiDark />light</div>
+                                            <CiDark />light
                                         </>
                                 }
                             </button>
