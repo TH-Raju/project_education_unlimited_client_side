c1b4133da76e27fdcf4c30d7a2d5055770589d0b
TH-Raju <rajukhan894200@gmail.com>
2022-10-26 19:14:51 +0600

    error section added on login page and register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -65,7 +65,7 @@ const Register = () => {
                     <label htmlFor="confirm_password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Confirm password</label>
                     <input type="password" name="confirm" id="confirm_password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••" required />
                 </div>
-                <p className='py-4 my-3 px-3 text-red-600 border border-red-500'>{errors}</p>
+                <p className='py-4 my-3 px-3 '>{errors}</p>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ml-24 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
                 <br />
                 <button type="button" onClick={handleGoogleSignIn} className="px-14 py-3 flex align-middle gap-5 w-full mt-6 text-center font-semibold border rounded-xl border-blue-900 dark:border-gray-100 dark:text-gray-100 hover:bg-gray-300"><FcGoogle className='text-2xl'></FcGoogle> Sign in with Google</button>
