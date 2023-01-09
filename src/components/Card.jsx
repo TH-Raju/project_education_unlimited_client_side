d84940dae738d3f699927929477225bfb9ad61b2
TH-Raju <rajukhan894200@gmail.com>
2023-01-09 18:53:36 +0600

    card design update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,7 +12,7 @@ const Card = ({ course }) => {
                     <div className="flex flex-col justify-between py-6 space-y-8">
                         <div className="space-y-2">
                             <h2 className="text-3xl font-semibold tracking-wide">{title}</h2>
-                            <p className="dark:text-gray-200">{body.length > 150 ? body.slice(' ', 150) : body}</p>
+                            <p className="dark:text-gray-200">{body.length > 150 ? body.slice(' ', 150) : body}...</p>
                         </div>
                         <p
                             className="text-2xl font-semibold hover:bg-sky-200 rounded-lg py-3 my-4">Price: $
