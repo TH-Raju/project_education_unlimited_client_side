4c0242d8e912a7bcf1916a8cab0ffb29d9113817
TH-Raju <rajukhan894200@gmail.com>
2023-01-09 19:11:51 +0600

    code cleanning

--- src/Home/WeAre.jsx
+++ src/Home/WeAre.jsx
@@ -4,7 +4,6 @@ const WeAre = () => {
     return (
         <div className='mb-32'>
             <h2 className="text-4xl text-center font-bold underline underline-offset-8">Meet Our Team</h2>
-
             <div className='flex flex-wrap gap-16 justify-center my-20'>
                 <div className='text-center'>
                     <img className="p-1 w-60 h-60 rounded-full ring-2 ring-cyan-400 dark:ring-gray-500" src={require('../image/pp.jpg')} alt="Bordered avatar" />
@@ -12,7 +11,6 @@ const WeAre = () => {
                     <p>Team Leader</p>
                 </div>
             </div>
-
             <div className='flex flex-wrap gap-16 justify-center my-20'>
                 <div className='text-center mt-6'>
                     <img className="p-1 w-52 h-52 rounded-full ring-2 ring-cyan-400 dark:ring-gray-500" src={require('../image/student-1.png')} alt="Bordered avatar" />
@@ -24,7 +22,6 @@ const WeAre = () => {
                     <p className='font-bold mt-4 text-xl'>Kaniz Fatema</p>
                     <p>Team Member</p>
                 </div>
-
                 <div className='text-center mt-6'>
                     <img className="p-1 w-52 h-52 rounded-full ring-2 ring-cyan-400 dark:ring-gray-500" src={require('../image/student-3.png')} alt="Bordered avatar" />
                     <p className='font-bold mt-4 text-xl'>Nazmun Nahar</p>
@@ -35,7 +32,6 @@ const WeAre = () => {
                     <p className='font-bold mt-4 text-xl'>Marjan Bushra</p>
                     <p>Team Member</p>
                 </div>
-
                 <div className='text-center mt-6'>
                     <img className="p-1 w-52 h-52 rounded-full ring-2 ring-cyan-400 dark:ring-gray-500" src={require('../image/student-5.png')} alt="Bordered avatar" />
                     <p className='font-bold mt-4 text-xl'>Abdullah Mahmud</p>
